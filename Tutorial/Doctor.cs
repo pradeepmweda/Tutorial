c3923c0e608caa158a506d03e16caf6a57d982c5
pramindu123 <pramindukumaranayake999@gmail.com>
2024-03-24 15:30:02 +0530

    Add project files.

---
+++ Tutorial/Doctor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial
+{
+    public class Doctor
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string specialization { get; set; }
+        public int Age {  get; set; }
+
+        public Doctor() 
+        {
+            
+        }
+
+        public Doctor(string aname, int aid, string aspecialization)
+        {
+            this.Name = aname;
+            this.Id = aid;
+            this.specialization = aspecialization;
+        }
+
+    }
+
+}
---
+++ Tutorial/Hospital.cs
@@ -0,0 +1,20 @@
+﻿namespace Tutorial
+{
+    internal class Hospital
+    {
+        static void Main(string[] args)
+        {
+            Doctor doctor1 = new Doctor("kavindu",2001,"aaa");
+            
+
+            Doctor doctor2 = new Doctor("Charith",2003,"bbb");
+
+            doctor2.Name = "pradeep";
+
+            Console.WriteLine(doctor1.Id);
+            Console.WriteLine(doctor2.Name);
+
+        }
+    }
+}
+
